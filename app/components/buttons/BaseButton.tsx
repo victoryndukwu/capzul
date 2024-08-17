1053e50ed4da3bd0eb03a5f0ce67953d0f7cef0f
Victory Ndukwu <victoryndukwu7@gmail.comm>
2024-08-17 10:32:08 +0100

    chore: add Button components

---
+++ app/components/buttons/BaseButton.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React, { ButtonHTMLAttributes } from 'react'
+import { cn } from '@/app/utils/helpers';
+import { poppins } from '@/app/utils/font';
+
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    className?: string,
+    children: React.ReactNode,
+    variant?: 'primary' | 'white' | 'pink' | 'disabled',
+}
+
+function Button({ children, className, variant = 'primary', ...props }: ButtonProps) {
+    return (
+        <button {...props} className={cn('py-3 px-6 rounded-[5px] font-semibold text-sm transition ease-in duration-200 text-white w-full',
+            {
+                "bg-primary hover:bg-navyBlueNormal": variant === 'primary',
+            }
+            ,
+            `${poppins.className}`,
+            className)}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
--- app/components/forms/SignupForm.tsx
+++ app/components/forms/SignupForm.tsx
@@ -2,17 +2,24 @@
 import React from 'react'
 import InputField from '../inputs/InputField'
 import PasswordField from '../inputs/PasswordField'
+import GoogleAuthButton from '../buttons/GoogleAuthButton'
+import BaseButton from '../buttons/BaseButton'
 
 function SignupForm() {
     return (
         <form>
-            <div className='grid grid-cols-2 grid-rows-3 gap-6 mt-10 mb-4'>
+            <div className='md:grid md:grid-cols-2 md:grid-rows-3 gap-6 mt-10 mb-4 flex flex-col'>
                 <InputField label='First name' />
                 <InputField label='Last name' />
                 <InputField label='Email or phone number' />
                 <PasswordField label='Password' />
                 <PasswordField label='Confirm Password' />
 
+            </div>
+
+            <div className='flex gap-6'>
+                <BaseButton>Create account</BaseButton>
+                <GoogleAuthButton> Sign-in with google</GoogleAuthButton>
             </div>
 
         </form>
